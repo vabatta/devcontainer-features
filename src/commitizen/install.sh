77ffa3ae7bc1a50960b9fab6100fa2ca17d7c3ec
Valerio Battaglia <vabatta@gmail.com>
2023-04-12 14:18:16 +0000

    feat: add `commitizen` feature

---
+++ src/commitizen/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Make sure pip3 is installed
+if ! command -v pip3 &> /dev/null
+then
+	echo "`pip3` not found... installing"
+	export DEBIAN_FRONTEND=noninteractive
+	apt-get update
+	apt-get -y install python3-pip
+	apt-get autoremove -y && apt-get clean -y
+fi
+
+# Install Commitizen and pre-commit
+echo "Installing `commitizen` and `pre-commit`..."
+pip3 install -U commitizen pre-commit
--- test/commitizen/test.sh
+++ test/commitizen/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# This test file will be executed against one of the scenarios devcontainer.json test that
-# includes the 'color' feature with "greeting": "hello" option.
+# This test file will be executed against an auto-generated devcontainer.json that
+# includes the 'commitizen' Feature with no options.
 
 set -e
 
@@ -10,7 +10,9 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "execute command" bash -c "hello | grep 'hello, $(whoami)!'"
+check "execute command" bash -c "pip3 -V | grep 'pip'"
+check "execute command" bash -c "cz version | grep -E '^[0-9]'"
+check "execute command" bash -c "pre-commit -V | grep 'pre-commit'"
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.

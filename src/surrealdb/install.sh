7f49533c332d86523ef47836abd497a4aad24e34
Valerio Battaglia <vabatta@gmail.com>
2023-04-16 18:16:56 +0000

    fix: add `wget` if `curl` not present

--- src/surrealdb/install.sh
+++ src/surrealdb/install.sh
@@ -3,8 +3,12 @@ set -e
 
 # Install surrealdb
 echo "Installing `surrealdb`..."
-curl --proto '=https' --tlsv1.2 -sSf https://install.surrealdb.com | sh
+if [ -v curl ]; then
+	curl --proto '=https' --tlsv1.2 -sSf https://install.surrealdb.com | sh
+else
+	wget --secure-protocol=TLSv1_2 -q https://install.surrealdb.com -O - | sh
+fi
 
 if [ ! -f "/usr/local/bin/surreal" ]; then
-	sudo mv ~/.surrealdb/surreal /usr/local/bin
+	mv ~/.surrealdb/surreal /usr/local/bin
 fi

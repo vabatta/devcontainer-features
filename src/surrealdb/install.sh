185fae600da96e5186e96960436f1550e888d620
vabatta <vabatta@gmail.com>
2023-04-16 20:23:33 +0200

    feat(#2): add `surrealdb` feature

---
+++ src/surrealdb/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Make sure `curl` is installed
+if [ ! -v curl ]; then
+	echo "`curl` not found... installing"
+	export DEBIAN_FRONTEND=noninteractive
+	apt-get update
+	apt-get -y install curl
+	apt-get autoremove -y && apt-get clean -y
+fi
+
+# Install surrealdb
+echo "Installing `surrealdb`..."
+curl --proto '=https' --tlsv1.2 -sSf https://install.surrealdb.com | sh
+
+if [ ! -f "/usr/local/bin/surreal" ]; then
+	mv ~/.surrealdb/surreal /usr/local/bin
+fi

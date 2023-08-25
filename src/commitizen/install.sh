e14a0a863dc6e5017b8b06ad7b57ebeb7ad8fb2b
vabatta <vabatta@gmail.com>
2023-08-25 08:24:16 +0200

    feat: remove backticks in comment

--- src/commitizen/install.sh
+++ src/commitizen/install.sh
@@ -4,7 +4,7 @@ set -e
 # Make sure pip3 is installed
 if ! command -v pip3 &> /dev/null
 then
-	echo "`pip3` not found... installing"
+	echo "pip3 not found... installing"
 	export DEBIAN_FRONTEND=noninteractive
 	apt-get update
 	apt-get -y install python3-pip
@@ -12,5 +12,5 @@ then
 fi
 
 # Install Commitizen and pre-commit
-echo "Installing `commitizen` and `pre-commit`..."
+echo "Installing commitizen and pre-commit..."
 pip3 install -U commitizen pre-commit

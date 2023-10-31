cbcfa802e2ac560adaf5948ca0c5ea148df19ae9
Valerio Battaglia <vabatta@gmail.com>
2023-10-31 14:54:32 +0000

    docs: improve the docs

--- test/bun/variant_debug-info.sh
+++ test/bun/variant_debug-info.sh
@@ -6,6 +6,7 @@ source dev-container-features-test-lib
 
 # Definition specific tests
 check "bun version" bash -c "bun --version | grep 0.5.1"
+# TODO: check if the build is `debug-info`
 
 # Report result
 reportResults

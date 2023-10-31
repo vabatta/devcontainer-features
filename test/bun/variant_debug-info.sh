9589c3a9f0a07f1c55b6441d844b5007ce72757d
Valerio Battaglia <vabatta@gmail.com>
2023-10-31 14:47:28 +0000

    test: fix tag and variant

--- test/bun/variant_debug-info.sh
+++ test/bun/variant_debug-info.sh
@@ -5,7 +5,7 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
-check "bun version" bash -c "bun --version | grep 0.1.4"
+check "bun version" bash -c "bun --version | grep 0.5.1"
 
 # Report result
 reportResults
